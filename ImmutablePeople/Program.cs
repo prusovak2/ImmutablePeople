94f267d985eee8c2c7717ba214630362aea3091e
prusovak2 <prusova.k2@seznam.cz>
2020-03-05 15:54:08 +0100

    Add project files.

---
+++ ImmutablePeople/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ImmutablePeople {
+	
+	class Program {
+		static void Main(string[] args) {
+			var studentA = Student.Default
+							.WithName("Pavel Rozek").WithPassword("lavice")
+							.WithDateEnrolled(new DateTime(2015, 9, 21));
+
+			var studentB = Student.Default
+							.WithName("Marie Pichova").WithPassword("zidle")
+							.WithDateEnrolled(new DateTime(2018, 9, 25));
+
+			var teacher = Teacher.Default
+							.WithName("Pavel Jezek").WithPassword("stul")
+							.WithCoursesHeld(5);
+
+			List<Person> people = new List<Person> { studentA, studentB, teacher };
+			Console.WriteLine("+++ People:");
+			people.PrintAll();
+			Console.WriteLine();
+
+			var updatedPeople = people.WithPasswordResetByFirstName(firstName: "Pavel", newPassword: "pohovka");
+			Console.WriteLine("+++ Updated people:");
+			updatedPeople.PrintAll();
+			Console.WriteLine();
+
+			Console.WriteLine("+++ People:");
+			people.PrintAll();
+			Console.WriteLine();
+		}
+	}
+
+}
